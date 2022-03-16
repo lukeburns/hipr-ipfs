1488b52326ed9f05715626a3bda8097633efb8e8
Luke Burns <lukemburns@gmail.com>
2022-03-16 11:16:59 -0400

    v0.5.0 - zone cache

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const fetchZone = require('./fetch-zone');
-const { wire: { types } } = require('bns');
+const { Zone, wire: { types, codes } } = require('bns');
+
+const empty = new Zone()
 
 function middleware () {
   const zones = new Map()
+  const add = (cid) => {
+    let zone = zones.get(cid)
+    if (zone) {
+      return zone
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      fetchZone(cid).then(zone => {
+        zones.set(cid, zone)
+        resolve(zone)
+      }).catch(reject)
+    })
+    zones.set(cid, promise)
+    return promise
+  }
   return {
     hostname: ':data.:protocol(_ipfs).:gateway?.', 
     handler: async ({ protocol, data }, name, type) => {
       data = data.split('.')
       const cid = data[data.length - 1]
       try {
-        let zone = zones.get(cid) 
-        if (!zone) {
-          try {
-            zone = await fetchZone(cid)
-            zones.set(cid, zone)
-          } catch (error) {
-            throw error
-          }
+        const zone = add(cid)
+        if (!(zone instanceof Promise)) {
+          return zone.resolve(name, types[type])
+        } else {
+          const res = empty.resolve(name, types[type])
+          res.code = codes.SERVFAIL // ensure response not cached
+          return res
         }
-        const res = zone.resolve(name, types[type])
-        return res
       } catch (e) {
-        console.log('error', e)
         return null
       }
     }
